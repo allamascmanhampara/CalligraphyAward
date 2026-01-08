c942307484d632581d5d80a50ad21058fdaf4428
allamascmanhampara <allamascmanhampara@gmail.com>
2026-01-09 04:13:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function showResults() {
   document.getElementById("landing").classList.add("hidden");
   document.getElementById("results").classList.remove("hidden");
 }
+
+function switchTab(tab) {
+  // buttons
+  document.querySelectorAll(".tab").forEach(b => b.classList.remove("active"));
+  event.target.classList.add("active");
+
+  // content
+  document.querySelectorAll(".tab-content").forEach(c => c.classList.add("hidden"));
+  document.getElementById(tab).classList.remove("hidden");
+}
